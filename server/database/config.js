74a92e643269f5a2dd1bb9a5130144d61ebdf770
Athul <athulpr147@gmail.com>
2023-11-24 14:08:04 +0530

    Mongo URI

--- server/database/config.js
+++ server/database/config.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', false)
 
 const connectDB = async()=>{
     try {
-        await mongoose.connect( "mongodb+srv://athulpr_WardrobeWhiz:l5oUJeLGBs0XSmK5@wardrobewhiz.maermy0.mongodb.net/WardrobeWhiz" || process.env.MongoDB_URI, {
+        await mongoose.connect( "mongodb+srv://athulpr_WardrobeWhiz:l5oUJeLGBs0XSmK5@wardrobewhiz.maermy0.mongodb.net/WardrobeWhiz", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
