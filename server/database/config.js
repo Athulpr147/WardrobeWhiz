e7a35f123ee999e71a4acab2a84aaab1bf9e466f
Athul <athulpr147@gmail.com>
2023-11-24 13:09:31 +0530

    Mongo URI

--- server/database/config.js
+++ server/database/config.js
@@ -4,7 +4,10 @@ mongoose.set('strictQuery', false)
 
 const connectDB = async()=>{
     try {
-         await mongoose.connect(process.env.MongoDB_URI)
+        await mongoose.connect(process.env.MongoDB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("Database connected")
     } catch (error) {
         console.log("Database is not connected Error :"+ error)
